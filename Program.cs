286db61877c47244bc318d905aa69aaeefe4f9ab
Евгений Кучма <desferto87@gmail.com>
2025-02-04 20:47:51 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace homeWSplit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string originalString = "Дана строка с текстом, используя метод строки String.Split() " +
+                  "получить массив слов, которые разделены пробелом " +
+                  "в тексте и вывести массив, каждое слово с новой строки.";
+            
+            string[] substringArray = originalString.Split(' ');
+
+            foreach (string substring in substringArray)
+            {
+                Console.WriteLine($"subs = {substring}");
+            }
+        }
+    }
+}
